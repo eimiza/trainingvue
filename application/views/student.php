493b5bb99f206ef617ccaf9926fa0aca0160e123
User2 <User2@gmail.com>
2025-04-24 12:30:22 +0800

    add modal, and bootstrap

--- application/views/student.php
+++ application/views/student.php
@@ -4,24 +4,39 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/admin-lte@3.1/dist/js/adminlte.min.js"></script>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.1/dist/css/adminlte.min.css">
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/vue@2.7.16/dist/vue.js"></script>
 </head>
 <body>
 	<div id="app">
 
+    <div class="modal fade" id="addDataModal" tabindex="-1" role="dialog" aria-labelledby="addDataModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="addDataModalLabel">Add Data</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
-
-					{{message}}
-                    {{data}}
-				
-                    <button @click="get_list()" class="btn btn-primary">Get Data</button>
+                    <button @click="show_add()" class="btn btn-primary">Add Data</button>
 				</div>
 			</div>
+            <br>
             <div class="row">
                 <div class="col-md-12">
                     <table class="table table-bordered">
@@ -65,6 +80,9 @@
 					alert(num);
 					this.visible = true;
 				},
+                show_add(){
+                    $('#addDataModal').modal('show');
+                },
                 get_list(){
                     var self = this;
                     $.post('student/api_student', function(res){
